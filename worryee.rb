32c071f06df8daefafdc5c022e15add87a3dbd67
Duncan Gough <duncan@suttree.com>
2014-08-26 14:22:33 +0100

    Using twilio to send SMS messages, rather than Twitter DMs

--- worryee.rb
+++ worryee.rb
@@ -1,8 +1,19 @@
 require 'rubygems'
-gem 'imessage'
+require 'twilio-ruby'
 
-if (1 + rand(10) == 5)
-  imessage send 'What are you worried about?' --to 'duncan@suttree.com'
+#if (1 + rand(10) == 5)
+if (true)
+  account_sid = 'AC2efb3573f29f88e3caf08568b6e1f061'
+  auth_token = '58a7025898ed039f92043d73c73d3f06'
+
+  client = Twilio::REST::Client.new account_sid, auth_token
+
+  client.account.messages.create(
+    :from => '+44 1908 410338',
+    :to => '+447740302248',
+    :body => 'What are you worried about?'
+  ) 
+  puts 'Sent worryee'
 else
-  puts "Nothing to worry about"
+  puts 'Nothing to worryee about'
 end
